ed8be0d57ab66446d39706f860bdad702e5a8093
Lars <144362461+laha0006@users.noreply.github.com>
2025-04-29 05:56:47 +0200

    testing

--- app.js
+++ app.js
@@ -5,12 +5,40 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+let playerStartFishing;
+app.get("/start", (req, res) => {
+    const now = Date.now();
+    playerStartFishing = now;
+    res.send({ time: now });
+});
+
+app.get("/stop", (req, res) => {
+    const now = Date.now();
+    const diff = now - playerStartFishing;
+    const seconds = diff / 1000;
+    res.send({ diff: diff, seconds: seconds });
+});
+
+function startFish(socket) {
+    socket.emit("FishingStarted");
+    socket.on("catch", (data) => {
+        console.log("Fish?");
+    });
+}
+
+const sessions = new Set();
 
 io.on("connection", (socket) => {
     console.log("A socket connected", socket.id);
 
     socket.on("custom", (data) => {
-        console.log(`custom: ${data}`);
+        console.log("custom!!");
+        if (sessions.has(socket.id)) {
+            console.log("return");
+            return;
+        }
+        sessions.add(socket.id);
+        startFish(socket);
     });
 
     socket.on("disconnect", () => {
