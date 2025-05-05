17de79f0f184900cb42e8967382b47b3e7084346
Lars <144362461+laha0006@users.noreply.github.com>
2025-05-05 05:53:17 +0200

    introduced better state

--- socket/fishing/fishingSession.js
+++ socket/fishing/fishingSession.js
@@ -1,6 +1,7 @@
 export function createFishingSession(socket) {
     return {
         socket,
+        fishingState: FishingState.IDLE,
         isFishing: false,
         isFishOnHook: false,
         isQTE: false,
@@ -13,32 +14,46 @@ export function createFishingSession(socket) {
     };
 }
 
+const FishingState = Object.freeze({
+    IDLE: "Idle",
+    FISHING: "Fishing",
+    WAITING_FOR_BITE: "WaitingForBite",
+    HOOKED: "Hooked",
+    QTE: "Qte",
+});
+
 export function startFishing(session) {
-    if (session.isFishing) return;
-    session.isFishing = true;
+    if (session.fishingState != FishingState.IDLE) return;
+
+    session.fishingState = FishingState.FISHING;
     scheduleNextFish(session);
 }
 
 function scheduleNextFish(session) {
+    if (session.fishingState != FishingState.FISHING) return;
+
     if (session.missCount >= 3) {
         console.log("missed too much stopped session");
         stopFishing(session);
         return;
     }
+
     const delay = Math.random() * 2000 + 2000;
     session.fishingTimer = setTimeout(() => {
+        session.fishingState = FishingState.WAITING_FOR_BITE;
         fishOnHook(session);
     }, delay);
 }
 
 function fishOnHook(session) {
-    if (!session.isFishing) return;
-    session.isFishOnHook = true;
+    if (session.fishingState != FishingState.WAITING_FOR_BITE) return;
+
     session.socket.emit("FishOnHook");
+    session.fishingState = FishingState.HOOKED;
 
     session.hookTimeout = setTimeout(() => {
-        if (session.isFishOnHook) {
-            session.isFishOnHook = false;
+        if (session.fishingState === FishingState.WAITING_FOR_BITE) {
+            session.fishingState = FishingState.FISHING;
             session.socket.emit("FishEscaped");
             session.missCount++;
             scheduleNextFish(session);
@@ -47,23 +62,26 @@ function fishOnHook(session) {
 }
 
 function sendQTE(session) {
-    if (!session.isFishing || session.isQTE) return false;
-    session.isQTE = true;
+    if (session.fishingState != FishingState.QTE) return;
+
     session.startQteTime = Date.now();
     session.socket.emit("QTE");
+
     session.qteTimer = setTimeout(() => {
-        if (session.isQTE) {
-            session.isQTE = false;
+        if (session.fishingState === FishingState.QTE) {
             session.socket.emit("FishEscaped");
+            session.fishingState = FishingState.FISHING;
             session.missCount++;
             scheduleNextFish(session);
         }
     }, 2500);
+
     return true;
 }
 
 export function qteComplete(session) {
-    if (!session.isFishing || !session.isQTE) return;
+    if (session.fishingState != FishingState.QTE) return;
+
     session.missCount = 0;
     clearTimeout(session.qteTimer);
 
@@ -72,63 +90,70 @@ export function qteComplete(session) {
         return;
     }
 
-    session.isQTE = false;
+    session.fishingState = FishingState.FISHING;
     session.socket.emit("QTESuccess");
+
     scheduleNextFish(session);
 }
 
 export function catchFish(session) {
-    if (!session.isFishing || !session.isFishOnHook) return;
+    if (session.fishingState != FishingState.HOOKED) return;
+
     clearTimeout(session.hookTimeout);
-    session.isFishOnHook = false;
+
     session.socket.emit("FishCaught");
+    session.fishingState = FishingState.QTE;
+
     const sent = sendQTE(session);
     if (!sent) {
         session.socket.emit("FishEscaped");
+        session.fishingState = FishingState.FISHING;
         session.missCount++;
         scheduleNextFish(session);
     }
 }
 
 export function stopFishing(session) {
-    session.isFishing = false;
+    session.fishingState = FishingState.IDLE;
+
     clearTimeout(session.fishingTimer);
     clearTimeout(session.hookTimeout);
     clearTimeout(session.qteTimer);
 }
 
 function isStdDevUnderThreshold(times) {
-    console.log("standard deviation check");
-    console.log(times.length);
     if (times.length < 10) {
-        console.log("below 10 times");
         return false;
     }
+
     const avg = times.reduce((sum, time) => sum + time, 0) / times.length;
-    console.log(times);
     const variance =
         times.reduce((sum, time) => sum + Math.pow(time - avg, 2), 0) /
         times.length;
+
     const stdDev = Math.sqrt(variance);
     if (stdDev < 20) return true;
+
     return false;
 }
 
 function isCheating(session) {
     const diff = Date.now() - session.startQteTime;
+    session.times.push(diff);
+
     if (diff < 200) {
         session.warnCount++;
     }
+
     if (session.warnCount > 2) {
-        console.log("too fast to be human bro");
         return true;
     }
-    session.times.push(diff);
+
     if (session.times.length > 10) {
         session.times.shift();
     }
+
     if (isStdDevUnderThreshold(session.times)) {
-        console.log("Too consistent for human");
         return true;
     }
 }
